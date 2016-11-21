d9489b0eb718a4138f9bf916447edeb394458e2b
sahilbansal17 <shlbnsl843@gmail.com>
2016-11-21 16:09:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ qpp.get('/article-two',function(req,res){
     res.send('Article-two requested and will be served here');
 });
 
-qpp.get('/article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('Article-three requested and will be served here');
 });
 
